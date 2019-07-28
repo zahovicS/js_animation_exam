a0b3513ea96c19593825c8734215ef4449c56c33
zahovicS <zv.elrevelde@gmail.com>
2019-07-28 10:47:49 -0500

    phaser

--- js/example.js
+++ js/example.js
@@ -1,1 +1,22 @@
-example.js
+let config=
+{
+	type: Phaser.WEBLG,
+	width: 800,
+	height: 600,
+	backgroundColor: '#2d2d2d',
+	parent: 'phaser-example',
+	scene:{
+		preload: preload,
+		create: create
+	}
+};
+
+let game= new Phaser.Game(config);
+function preload(){
+	this.load.image('bg','../img/escenario.jpg');
+	this.load.image('ovni','../img/ovni.png',{
+		frameWidth: 136, frameHeight:80});
+}
+function create(){
+	this.add.image(400,300,'bg');
+}
